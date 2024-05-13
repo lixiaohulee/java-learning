9f51526d73aa51d20ef9ae32d1b1be5ea6b76017
lixiaohulee <lixiaohu_neuq@163.com>
2024-05-13 22:39:01 +0800

    xml

---
+++ src/main/java/com/xml/Main.java
@@ -0,0 +1,18 @@
+package com.xml;
+
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Main {
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+        InputStream input = Main.class.getResourceAsStream("./book.xml");
+        SAXParserFactory spf = SAXParserFactory.newInstance();
+        SAXParser saxParser = spf.newSAXParser();
+        saxParser.parse(input, new MyHandler());
+    }
+}
